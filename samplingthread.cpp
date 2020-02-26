102ecc42501c9a620a01b7c390312e4d0f6a024f
oz-matt <matt.morra@ozoralabs.com>
2020-02-26 16:35:19 -0500

    Testing thruput speed

--- samplingthread.cpp
+++ samplingthread.cpp
@@ -56,88 +56,17 @@ DWORD lastAddress = 0;
 
 void SamplingThread::sample( double elapsed )
 {
-    if ( d_frequency > 0.0 )
+
+    if(cpi.Get_BRAM_Address_Pointer(&address))
     {
-        int i;
-        for(i=0;i<DATA_PTS_COLLECTED_PER_SAMPLE_FXN_CALL;i++){
-
-            DWORD address = 0;
-
-            if(cpi.Get_BRAM_Address_Pointer(&address))
-            {
-                if(cpi.updateOscData())
-                {
-                    bool wrap = false;
-                    int wrap_spacing = 0;
-
-                    //qDebug("Address: %i, Data: %d", address, cpi.pcie_read_data[address]);
-                    int numNewPoints = 0;
-
-                    if((signed int)(address - lastAddress) < 0)
-                    {
-                        wrap = true;
-                        wrap_spacing = 16384 - lastAddress;
-                        numNewPoints = wrap_spacing + address + 1;
-                    }
-                    else
-                    {
-                        numNewPoints = address - lastAddress;
-                    }
-
-                    //qDebug("Num Points To Update: %d, wrap: %b", numNewPoints, wrap);
-                    lastAddress = address;
-
-                    if (wrap)
-                    {
-                        int j, k;
-                        for(j=(wrap_spacing-1);j>=0;j--)
-                        {
-                            const QPointF s( curr_time, cpi.pcie_read_data[16384 - j]);
-                            SignalData::instance().append( s );
-                            curr_time = curr_time + TIMESTEP;
-                        }
-                        for(k=address;k>=0;k--)
-                        {
-                            const QPointF s( curr_time, cpi.pcie_read_data[address - k]);
-                            SignalData::instance().append( s );
-                            curr_time = curr_time + TIMESTEP;
-                        }
-                    }
-                    else
-                    {
-                        qDebug("\r\nSample START:\r\n\r\n");
-                        qDebug("Num Points To Update: %d", numNewPoints);
-
-                        int j;
-                        for(j=(numNewPoints-1);j>=0;j--)
-                        {
-                            DWORD y = (cpi.pcie_read_data[address - j]) >> 48;
-                            double yvolts = (y - 8192) * 0.01220703125;
-
-                            const QPointF s( curr_time, yvolts);
-                            SignalData::instance().append( s );
-                            curr_time += TIMESTEP;
-                            qDebug("Address: %d, Data: %hd, Volts: %f", address, y, yvolts);
-                        }
-
-                        qDebug("\r\nEarlier:");
-                        qDebug("Address: %d, Data: %llX", address - j - 2, cpi.pcie_read_data[address - j - 2]);
-                        qDebug("Address: %d, Data: %llX", address - j - 3, cpi.pcie_read_data[address - j - 3]);
-                        qDebug("Address: %d, Data: %llX", address - j - 4, cpi.pcie_read_data[address - j - 4]);
-                    }
-                }
-                else
-                {
-                    qDebug("FAILED TO GET DATA!!!");
-                }
-
-            }
-            else
-            {
-                qDebug("FAILED TO GET ADDRESS!!!");
-            }
-        }
+        qDebug("Address: %d, NewPts: %d", address, address - lastAddress);
+        lastAddress = address;
     }
+    else
+    {
+        qDebug("FAILED TO GET ADDRESS!!!");
+    }
+
 }
 
 int i = 0;
